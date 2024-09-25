26a6be9ad738239051be08c01a48b4152d18ec96
willsterling23 <willsterling23@gmail.com>
2024-09-25 08:03:48 -0500

    Initial player controller work

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    private PlayerControls _playerControls;
+
+    private void Awake()
+    {
+        _playerControls = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        _playerControls.Enable();
+    }
+
+    private void Update()
+    {
+        var x = _playerControls.Player.Move.ReadValue<Vector2>().x;
+        var z = _playerControls.Player.Move.ReadValue<Vector2>().y;
+    }
+    
+    
+}
