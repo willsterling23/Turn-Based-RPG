cba7faa42ace28fed0d6d20f9efb63f622fd16b6
willsterling23 <willsterling23@gmail.com>
2024-09-27 07:16:34 -0500

    Player Controller and Sprites
    - Added movement to player controller
    - Added player sprite

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private int _playerSpeed;
     
+    private Rigidbody _playerRigidbody;
+    private Vector3 _movement;
     private PlayerControls _playerControls;
 
     private void Awake()
@@ -18,11 +21,22 @@ public class PlayerController : MonoBehaviour
         _playerControls.Enable();
     }
 
+    private void Start()
+    {
+        _playerRigidbody = GetComponent<Rigidbody>();
+    }
+
     private void Update()
     {
         var x = _playerControls.Player.Move.ReadValue<Vector2>().x;
         var z = _playerControls.Player.Move.ReadValue<Vector2>().y;
+        
+        _movement = new Vector3(x, 0, z).normalized;
+        
     }
-    
-    
+
+    private void FixedUpdate()
+    {
+        _playerRigidbody.MovePosition(transform.position + _movement * (_playerSpeed * Time.fixedDeltaTime));
+    }
 }
